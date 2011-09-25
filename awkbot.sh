95de1704ff96b917a8e62a5d66804ec3858bd102
AWK Robot <lamo@lamo.su>
2011-09-25 19:09:19 +0300

    add nonblock for normal awk operation

--- awkbot.sh
+++ awkbot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-socat -b 100 -v TCP6:irc.meganet.ru:7770 SYSTEM:./awkbot.awk,pty,echo=0 >> awkbot.log 2>&1
+socat -v TCP6:irc.meganet.ru:7770,nodelay,nonblock SYSTEM:./awkbot.awk,pty,echo=0 >> awkbot.log 2>&1
 
